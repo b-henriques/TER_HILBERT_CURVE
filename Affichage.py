dd7c980e68a1e12ce7185280b9b966b7e76ed963
ryanG394 <77676509+ryanG394@users.noreply.github.com>
2022-04-06 10:53:58 +0200

    Update Affichage.py

--- Affichage.py
+++ Affichage.py
@@ -1,2 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
+
+x = np.array ()    # Données en abscisse 
+y = np.array ()    # Données en ordonnée
+
+plt.plot(x, y, 'x')     # Tracé de la courbe
+plt.title('Mon titre')  # Ajout d'un titre
+plt.xlabel('x')         # Nom de la grandeur en abscisse
+plt.ylabel('y')         # Nom de la grandeur en ordonnée
+plt.grid()              # Ajout d'une grille
+plt.show()              # Affichage
