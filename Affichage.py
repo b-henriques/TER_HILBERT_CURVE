55a6b9853f126a8aff6f4a57014561c5ceb9c4fc
ryanG394 <77676509+ryanG394@users.noreply.github.com>
2022-04-07 15:20:19 +0200

    Exemple pybind

--- Affichage.py
+++ Affichage.py
@@ -1,6 +1,47 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+cmake_minimum_required(VERSION 3.10)
+project(pybind_vid)
+add_subdirectory(pybind11)
+pybind11_add_module(module_name main.cpp) // le nom du module et la bonne fonction
+
+#include <pybind11/pydind11.h>
+
+namespace = pybind11;
+
+class UneClasse {
+    float multiplier;
+
+    public :
+        float multiply(float input){
+            return multiplier * input;
+        };
+    
+    private : 
+
+}
+
+PYBIND11_MODULE(module_name, handle){
+    handle.doc() = "This is the module docs";
+    handle.def("nom_fonction_en_python", &nom_fonction);
+
+    py::class_<SomeClass>(
+                            handle, "PySomeClass"
+                        )
+        .def(py::init<)
+}
+
+
+// print(nom_fonction_en_python(3,5))
+
+//print(PySomeCLass)
+
+
+
+
+
+
 x = np.array ()    # Données en abscisse 
 y = np.array ()    # Données en ordonnée
 
@@ -10,3 +51,4 @@ plt.xlabel('x')         # Nom de la grandeur en abscisse
 plt.ylabel('y')         # Nom de la grandeur en ordonnée
 plt.grid()              # Ajout d'une grille
 plt.show()              # Affichage
+
